5b4c9d9c0c40b636b43b87a3bf24c0cf1b86f8fa
guorong <guorong@tuyoogame.com>
2022-04-11 16:38:07 +0800

    ant design vue按需加载

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from "./store/index"
+import { Button, message } from 'ant-design-vue';
+import 'ant-design-vue/es/message/style/css';
 let app = createApp(App)
+
+// 全局注册组件
+app.config.globalProperties.$message = message;
+app.use(Button)
 app.use(store)
 app.mount('#app')
